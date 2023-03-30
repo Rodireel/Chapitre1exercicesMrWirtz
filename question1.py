a007f8d30fbe5f40efa7d70ffb98f7fc0864cd7c
Rodireel <124796746+Rodireel@users.noreply.github.com>
2023-03-30 02:31:38 +0200

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,1 +1,6 @@
 import pandas as pd
+#une fois pandas installé, on trouve le chemin d'accès de bc.csv pour utiliser la commande read_csv
+df=pd.read_csv("/Users/rodibaran/Downloads/NoSQL-master/data/Chap1/bc.csv")
+#il ne reste plus qu'à afficher l'objet "df"
+df
+#Nous constatons un nombre de lignes egal à 569, pour 33 colonnes.
