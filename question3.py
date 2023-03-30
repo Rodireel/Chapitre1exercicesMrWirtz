df884b4a7a6eb6f26b96c27732709424bef42a8c
Rodireel <124796746+Rodireel@users.noreply.github.com>
2023-03-30 03:13:35 +0200

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,10 @@
+#Il faut dès à présent ajouter des valeurs à la base de données
+import csv
+import sqlite3
+#On retrace le chemin d'accès
+conn=sqlite3.connect('/Users/rodibaran/Downloads/NoSQL-master/data/Chap1/bc.db')
+c=conn.cursor()
+#On met en place les nouveaux éléments de la base de données
+c.execute('CREATE TABLE tomatch (id_ INTEGER PRIMARY KEY, rdm_float)')
+conn.commit
+conn.close
