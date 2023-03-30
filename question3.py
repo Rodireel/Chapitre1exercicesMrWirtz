2eb81c529458e6129d5594b52e0775cf1a34d814
Rodireel <124796746+Rodireel@users.noreply.github.com>
2023-03-30 03:18:05 +0200

    Update question3.py

--- question3.py
+++ question3.py
@@ -5,6 +5,6 @@ import sqlite3
 conn=sqlite3.connect('/Users/rodibaran/Downloads/NoSQL-master/data/Chap1/bc.db')
 c=conn.cursor()
 #On met en place les nouveaux éléments de la base de données
-c.execute('''CREATE TABLE tomatch (id_ INTEGER PRIMARY KEY, rdm_float)''')
+c.execute('CREATE TABLE tomatch (id_ INTEGER PRIMARY KEY, rdm_float)')
 conn.commit
 conn.close
