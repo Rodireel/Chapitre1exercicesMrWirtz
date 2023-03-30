33dc4670290692fbeb208da4032fe41fda81ff76
Rodireel <124796746+Rodireel@users.noreply.github.com>
2023-03-30 07:41:27 +0200

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,8 +1,9 @@
 import random
 import sqlite3
 conn = sqlite3.connect('/Users/rodibaran/Downloads/NoSQL-master/data/Chap1/bc.db')
+c = conn.cursor()
 #Les conditions
-conditions=[(i, random.random()) for i in range (50000, 10000001, 2)]
-c-conn.cursor()
-c.executemany('INSERT INTO tomatch (id_, rdm_float) VALUES (?,?)', conditions)
+for i in range(50000, 10000001, 2):
+ c.execute('INSERT INTO tomatch (id_, rdm_float) VALUES (?, ?)', (i, rdm_float))
 conn.commit()
+conn.close
