355eaaaadfdda11c686de895f55bd9e73be3309e
Rodireel <124796746+Rodireel@users.noreply.github.com>
2023-03-30 07:34:46 +0200

    Create question4.py

---
+++ question4.py
@@ -0,0 +1,8 @@
+import random
+importsqlite3
+conn = sqlite3.connect('/Users/rodibaran/Downloads/NoSQL-master/data/Chap1/bc.db')
+#Les conditions
+conditions=[(i, random.random()) for i in range (50000, 10000001, 2)]
+c-conn.cursor()
+c.executemany('INSERT INTO tomatch (id_, rdm_float) VALUES (?,?)', conditions)
+conn.commit()
